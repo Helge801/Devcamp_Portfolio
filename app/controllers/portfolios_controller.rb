ae016f5d20b8ec4449f89ee6a32494dbf9ab6b2d
Bryan Lund <bryan.helge@gmail.com>
2017-12-12 12:30:38 -0700

    Created permission structure

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :show, :destroy, :update]
   layout "portfolio"
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   def show
   end
