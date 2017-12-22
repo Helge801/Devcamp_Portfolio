48032251749df210725aeb38aeb7ad3ae4d40d5f
Bryan Lund <bryan.helge@gmail.com>
2017-12-22 13:54:28 -0700

    Checkout urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://helge-devcamp-portfolio.herokuapp.com/', 'http://helge-devcamp-portfolio.herokuapp.com/']
-config.action_cable.url = "wss://helge-devcamp-portfolio.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://helge-devcamp-portfolio.herokuapp.com', 'http://devcamp-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://helge-devcamp-portfolio.herokuapp.com/cable"
 end
 
