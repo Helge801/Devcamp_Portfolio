b923c62b84baa425f35ec0b12924cc941fa15f6a
Bryan Lund <bryan.helge@gmail.com>
2017-12-29 20:29:00 -0700

    background particals using particals.js and canvas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,7 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+//= require particles.js
 
 
   $(document).ready(function() {
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( blogs.scss )
 Rails.application.config.assets.precompile += %w( portfolios.scss )
+
+Rails.application.config.assets.precompile += %w( particles.js )
+Rails.application.config.assets.precompile += %w( app.js )
+Rails.application.config.assets.precompile += %w( style.css )
