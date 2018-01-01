150f088a9470df9c9150434f767762ae0f633e22
Bryan Lund <bryan.helge@gmail.com>
2017-12-31 18:08:48 -0700

    fixed scrolling anchor issue

--- app/assets/javascripts/anim.js
+++ app/assets/javascripts/anim.js
@@ -1,6 +1,28 @@
-  $(function() {
-    $("a[href^='#']").on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 300, 'easeInOutQuad');
-    });
+  // $(function() {
+  //   $("a[href^='#']").on('click', function(e) {
+  //     console.log('working');
+  //     $('body').animate({
+  //       scrollTop: $($(this).attr('href')).offset().top }, 300, 'easeInOutQuad');
+  //     e.preventDefault();
+  //   });
+  // });
+
+
+$(function() {
+  $("a[href^='#']").click(function(e) {
+       var elementClicked = $(this).attr("href");
+       var destination = $(elementClicked).offset().top;
+       $("body").animate({ scrollTop: parseInt($("body").scrollTop() + destination)} );
+       e.preventDefault();
   });
+});
+
+
+//   $('a').click(function(e){
+//     e.preventDefault();
+//     console.log('default stopped');
+//     $('html, body').animate({
+//         scrollTop: $( $(this).attr('href') ).offset().top
+//     }, 500);
+//     return false;
+// });
