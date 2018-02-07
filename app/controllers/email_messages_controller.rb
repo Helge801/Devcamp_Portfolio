5841f3350c4afe50290090080bdb46e12dc36c67
Bryan Lund <bryan.helge@gmail.com>
2018-02-06 21:49:24 -0700

    set system to save email for now

--- app/controllers/email_messages_controller.rb
+++ app/controllers/email_messages_controller.rb
@@ -2,7 +2,8 @@ class EmailMessagesController < ApplicationController
 
 def create
   new_email = EmailMessage.new(email_params)
-  UserMailer.message_email(new_email.address,new_email.message).deliver_later
+  # UserMailer.message_email(new_email.address,new_email.message).deliver_later
+  new_email.save
   redirect_to root_path
 end
 
